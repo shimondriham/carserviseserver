c2866f078887cfab3c2a6bbf77bb6b902064e59e
shimon driham <95618903+shimondriham@users.noreply.github.com>
2022-10-11 18:16:46 +0300

    first commit

---
+++ db/mongoConnect.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const {secret} = require("../config/config")
+
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect(`mongodb+srv://${secret.userDb}:${secret.passDb}@cluster0.frzne.mongodb.net/car`);
+  // await mongoose.connect(`mongodb+srv://${secret.userDb}:${secret.passDb}@cluster0.cmu9u.mongodb.net/car`);
+  console.log("mongo connected shimon count to car chanel!!!");
+}
