e70697ec73a666c65c457e60e6b10eaa58de13ca
shimon driham <95618903+shimondriham@users.noreply.github.com>
2022-11-16 00:00:17 +0200

    test1

--- db/mongoConnect.js
+++ db/mongoConnect.js
@@ -6,5 +6,5 @@ main().catch(err => console.log(err));
 
 async function main() {
   await mongoose.connect(`mongodb+srv://${secret.userDb}:${secret.passDb}@cluster0.frzne.mongodb.net/car`);
-  console.log("mongo connected shimon count to car chanel!!!");
+  console.log("mongo connected shimon to car !!!");
 }
